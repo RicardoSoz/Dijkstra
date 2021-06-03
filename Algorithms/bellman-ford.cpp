0bde1c9f6921752a0d780301563b10575d3413e1
RicardoSoz <ricardosoz7@protonmail.com>
2021-06-02 21:46:51 -0500

    First Commit

--- Algorithms/bellman-ford.cpp
+++ Algorithms/bellman-ford.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include<string>
+#include <fstream>
+#include <iomanip>   
 
 using namespace std;
     
 // Given an Adjacency List, find all shortest paths from "start" to all other vertices.
-vector<int> BellmanFordSP(vector< vector<pair<int, int> > > &adjList, int &start)
-    {
+vector<int> BellmanFordSP(vector< vector<pair<int, int> > > &adjList, int &start){
     cout << "\nGetting the shortest path from " << start << " to all other nodes.\n";
     vector<int> dist;
-    
+
     // Initialize all source->vertex as infinite.
     int n = adjList.size();
-    for(int i = 0; i < n; i++)
-        {
-        dist.push_back(1000000007); // Define "infinity" as necessary by constraints.
-        }
+    for(int i = 0; i < n; i++){
+      dist.push_back(1000000007); // Define "infinity" as necessary by constraints.
+    }
         
     dist[start] = 0;    
     
     // Then calculate the shortest distance using...
     // For numNodes-1...
-    for(int i = 0; i < n-1; i++)
-        {
+    for(int i = 0; i < n-1; i++){
         // For each node (u)...
-        for(int u = 0; u < n; u++)
-            {
+        for(int u = 0; u < n; u++){
             // For each of it's neighbors (v)...
-            for(int j = 0; j < adjList[u].size(); j++)
-                {
+            for(int j = 0; j < adjList[u].size(); j++){
                 int v = adjList[u][j].first;
                 int weight = adjList[u][j].second;
                 
                 // If the distance from source to v is bigger than dist[u] + weight of (u,v)...
-                if(dist[v] > dist[u] + weight)
-                    {
+                if(dist[v] > dist[u] + weight){
                     // Update dist[v] to dist[u] + weight(u,V)
                     dist[v] = dist[u] + weight;
-                    }
                 }
             }
-        }            
+        }
+    }            
     // If there's a negative weight cycle in the graph, then report it, by...
     // For each node (u)...
         // For each of u's neighbors (v)...
@@ -51,24 +48,30 @@ vector<int> BellmanFordSP(vector< vector<pair<int, int> > > &adjList, int &start
     return dist;
     }
     
-void PrintShortestPath(vector<int> &dist, int &start)
-    {
-    cout << "\nPrinting the shortest paths for node " << start << ".\n";
-    for(int i = 0; i < dist.size(); i++)
-        {
-        cout << "The distance from node " << start << " to node " << i << " is: " << dist[i] << endl;
-        }
+void PrintShortestPath(vector<int> &dist, int &start){
+    int op;
+    ofstream MyFile("Resultados_Test.txt");
+    MyFile << "Printing the shortest paths for node "<< start <<endl;
+    for(int i = 0; i < dist.size(); i++){
+        op++;
+        if(dist[i]==1000000007){continue;}
+        MyFile << "The distance from node " << start << " to node " << i << " is: " << dist[i] << endl;   
     }
+    cout << "Total of operations: " << op <<endl;
+    MyFile << "Total of operations: " << op <<endl;
+}
     
-int main() 
-    {
+int main(){
+
+    time_t start, end;
+    time(&start);
+
     cout << "Program started.\n";
     
     // Our adjacency list.
     vector< vector<pair<int, int> > > adjList;
     vector< pair <int,int> > vPairs;
     
-    // We have 7 vertices, so initialize 7 rows.
     int n, c, r;
     cin >> n;
 
@@ -81,20 +84,21 @@ int main()
     cin >> c;
     cin >> r;
 
-    int inputMatrix[r][c];
+    vector<int> inputMatrix;
+    int k;
+    int rsqr = r*2;
 
-    for (int i = 0; i < r; i++){
-      for(int j = 0; j < c; j++){
-        cin >> inputMatrix[i][j];
-      }
+    for (int i = 0; i < rsqr; i++){
+        cin>> k;
+        inputMatrix.push_back(k); 
     }
 
-    int* start = &inputMatrix[0][0];
-    vector<int> vMatrix(start, start + (c * r));
-
+    int range2 = rsqr;
     //The first int is the vertex of the friend, the second int is the edge weight.
-    for (int i = 0; i < r; i++){
-      vPairs.push_back(make_pair(inputMatrix[i][0],inputMatrix[i][1]));
+    for (int i = 0, j = 1; i < rsqr ; i++, j++){
+      //cout << inputMatrix[j];
+      vPairs.push_back(make_pair(inputMatrix[i],inputMatrix[j]));
+      i++; j++;
     }
 
     for(int i = 0; i < n; i++){
@@ -108,16 +112,22 @@ int main()
       for(int j = 0; j < appearance[i]; j++){
         adjList[i].push_back(vPairs[p]);
         p++;
-      }
-    
+        }  
     // Get a list of shortest path distances for node 0.
     int node = 0;
     vector<int> dist = BellmanFordSP(adjList, node);
     
     // Print the list.
     PrintShortestPath(dist, node);
+
+    time(&end);
+
+    double time_taken = double(end - start);
+    cout << "Time taken by program is : " << fixed
+         << time_taken << setprecision(10);
+    cout << " sec " << endl;
     
     cout << "Program ended.\n";
     
     return 0;
-    }
+}
--- GraphCreator/graph.cpp
+++ GraphCreator/graph.cpp
@@ -5,46 +5,58 @@
 #include<vector>
 #include<numeric>
 #include <cmath>
+#include <ctime>
 
 using namespace std;
 
-void createGraph();
+void createGraph(int nodes, int edges, int nTest);
 
 int main(){
-  createGraph();
+  int nodes, edges, nTest;
+  cin >> nodes;
+  cin >> edges;
+  cin >> nTest;
+  createGraph(nodes, edges, nTest);
   return 0;
 }
 
-void createGraph(){
-    ofstream MyFile("filename.txt");
+void createGraph(int nodes, int edges, int nTest){
+    string test = "Test_" + to_string(nTest) + ".txt";
+    ofstream MyFile(test);
     srand((unsigned) time(0));
 
-    auto range = 15000; 
-    auto numberNodes = 10000 + (rand() * range) / RAND_MAX;
-    auto edges = numberNodes/3;
+    //Number of Nodes
+    auto numberNodes = nodes;
+    //Number of Edges for every node
+    auto numberEdges = edges;
+    //Vector with each connection
     vector<int> nodesEdges;
 
-    MyFile <<numberNodes<<endl;
+    //Write in the Txt the number of nodes;
+    MyFile << numberNodes <<endl;
 
-    for(int i = 0; i<numberNodes; i++){
-      auto numEdges = (rand() % edges) + 1;
+    //Create how many edges every node has and write them in the Txt
+    for(int i = 0; i < numberNodes; i++){
+      auto qEdges = (rand() % numberEdges) + 1;
 
-      nodesEdges.push_back(numEdges);
+      nodesEdges.push_back(qEdges);
 
-      MyFile <<numEdges<<" ";
+      MyFile <<qEdges<<" ";
     }
 
     MyFile<<endl;
-      
+
+    //The total of Edges  
     long accumulateEdgesSum = accumulate(nodesEdges.begin(),nodesEdges.end(),0);
 
     MyFile<<2<<endl;
     MyFile<<accumulateEdgesSum<<endl;
 
+    //Create the connection with its weight and write it down in the text
     for(int i = 0; i<accumulateEdgesSum; i++){
       auto tmp = rand() / (RAND_MAX - 1.);
       auto node =(rand() % numberNodes);
-      auto weight =(rand() % edges) + 1;;
+      auto weight =(rand() % 10) + 1;;
 
       MyFile<<node<<" "<<weight<<endl;
     }
